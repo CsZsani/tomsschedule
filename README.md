1dd95bd8151d9968b324cc256c83c644868d09f0
CsZsani <zsihr8@inf.elte.hu>
2022-01-22 17:08:50 +0100

    Adding base code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tom's Schedule
+Time management Android app made with Java.
---
+++ app/src/main/java/hu/janny/tomsschedule/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package hu.janny.tomsschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+        //setContentView(R.layout.activity_splash_screen);
+    }
+}
